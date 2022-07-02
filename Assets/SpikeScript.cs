e49ece23436e2ccc2874634be2d50a890d715baf
LennartZastrow <83474423+LennartZastrow@users.noreply.github.com>
2022-07-02 17:29:23 +0200

    Final_Project_Unity_RunBoy

---
+++ Assets/SpikeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikeScript : MonoBehaviour
+{
+    public AudioSource hitSound;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        hitSound.Play();
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+    }
+}
+
